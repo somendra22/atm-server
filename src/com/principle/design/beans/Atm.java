000d0b2d9310cebfdb1f5955cf331149a7dc65e8
Somendra Agrawal <somendra.agrawal@talentica.com>
2016-07-29 12:29:10 +0530

    code base

--- src/com/principle/design/beans/Atm.java
+++ src/com/principle/design/beans/Atm.java
@@ -2,7 +2,38 @@ package com.principle.design.beans;
 
 public class Atm {
 
-	private int lat;
+	private int latitude;
 	private int longitude;
-	private int a; 
+	private int balanceAmount;
+
+	public Atm(int latitude, int longitude, int balanceAmount) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.balanceAmount = balanceAmount;
+	}
+
+	public int getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(int longitude) {
+		this.longitude = longitude;
+	}
+
+	public int getBalanceAmount() {
+		return balanceAmount;
+	}
+
+	public void setBalanceAmount(int balanceAmount) {
+		this.balanceAmount = balanceAmount;
+	}
+
 }
